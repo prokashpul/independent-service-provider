eab8c0e356c2747c0a9a92225187d33fb1587a0c
prokash <pprokash102@gmail.com>
2022-04-17 02:13:14 +0600

    google authontication add

--- src/Components/Pages/Authentication/SocialLogin/SocialLogin.js
+++ src/Components/Pages/Authentication/SocialLogin/SocialLogin.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../../../firebase.init";
 import "./SocialLogin.css";
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
+  if (user) {
+    toast("loading...");
+    navigate("/");
+  }
+  if (error) {
+    toast(error.message);
+  }
+
   return (
     <div>
       <div className="more-option">
@@ -9,7 +23,11 @@ const SocialLogin = () => {
         <span></span>
       </div>
       <div className="social-login">
-        <div className="google" title="Google Login">
+        <div
+          onClick={() => signInWithGoogle()}
+          className="google"
+          title="Google Login"
+        >
           <img src="https://i.ibb.co/cvbHMw3/Google.png" alt="google" />
         </div>
         <div className="facebook" title="Facebook Login">
