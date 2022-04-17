4e712df86f15d8140e6d1c64b4b7e55bc1d1930d
prokash <pprokash102@gmail.com>
2022-04-17 06:25:09 +0600

    dynamic page tytle set

--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Title from "../../../../Utilities/DynamicTitle";
 import Banner from "../Banner/Banner";
 import "./Home.css";
 const Home = () => {
+  Title("ProWeb wedding photographer");
   return (
     <div className="home">
       <Banner></Banner>
